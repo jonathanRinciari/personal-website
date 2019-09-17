00ac239d9d6e5e722cb9def7d88b646170bf0e82
jon rinciari <jonrinciari@jons-MacBook-Air.local>
2019-09-17 07:23:43 -0400

    fix: initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Header from './pages/Header/Header';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={`
+      background: red;
+    `}>
+      <Header  />
+      <Switch>
+        <Route exact path='/' component={Home} ></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <span> HOme</span>
+    </div>
+  );
+}
+
+export default Home;
