2dc20c3d72703cb0b3ee4e0e29775ec1815925af
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-25 23:10:10 -0400

    updated urls

--- src/projects.js
+++ src/projects.js
@@ -1,18 +1,18 @@
 const MyProjects = [
   {
-    url: "",
+    url: "yelp-api",
     title: "YelpApi",
     subtext: "Nativescript Plugin",
     logo: ""
   },
   {
-    url: "",
+    url: "react-native-swipeable-cards",
     title: "React Native Swipeable Cards",
     subtext: "Component",
     logo: ""
   },
   {
-    url: "",
+    url: "angular-grid-directive",
     title: "Angular Grid Directive",
     subtext: "Angular Utility",
     logo: ""
---
+++ src/writings.js
@@ -0,0 +1,28 @@
+const MyWritings = [
+    {
+      url: "angular-custom-async-validators",
+      title: "Angular: Custom Async Validators",
+      subtext: "778 words",
+      logo: ""
+    },
+    {
+      url: "ngrx-state-management-overview",
+      title: "NGRX State Management — Overview",
+      subtext: "707 words",
+      logo: ""
+    },
+    {
+      url: "angular-reactive-form-validation",
+      title: "Angular 6 Reactive Form Validation",
+      subtext: "1032 words",
+      logo: ""
+    },
+    {
+        url: "surgical-consultant-to-full-stack-developer",
+        title: "Surgical Consultant To Full Stack Developer",
+        subtext: "878 words",
+        logo: ""
+      }
+  ];
+  
+  export default MyWritings
