4f3a6d6d03fd284d046436b304cefb1f390620c4
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-26 22:28:55 -0400

    added category divider for projects

---
+++ src/pages/Project/CategoryDivider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const CategoryDivider = ({}) => {
+    return (
+        <div css={`
+        background: rgba(0,0,0,0.1);
+        height: .35em;
+        border-radius: 10px;
+        margin-bottom: 1rem;
+    `}></div>
+    )
+}
+
+export default CategoryDivider;
