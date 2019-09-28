a5dce3171bc8a414f60006292a6de90ba3daecbe
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-27 22:32:19 -0400

    finished info

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -6,9 +6,13 @@ import CategoryDivider from "./CategoryDivider";
 import SummaryCard from "../../components/SummaryCard";
 import Footer from "../../components/Footer";
 import SupportBlock from "./SupportBlock";
+import AnchorButton from "./AnchorButton";
 
 class Project extends Component {
   render() {
+    const project = this.props.location.state.project
+      ? this.props.location.state.project
+      : {};
     return (
       <div>
         <div>
@@ -24,7 +28,7 @@ class Project extends Component {
               margin: 0
             }}
           >
-            TEST
+            {project.title}
           </BodyText>
           <BodyText
             sizer={1.75}
@@ -38,28 +42,98 @@ class Project extends Component {
               marginBottom: "2rem"
             }}
           >
-            BANANA
+            {project.subtext}
           </BodyText>
         </div>
         <SectionDivider />
         <BodyText
-            sizer={2.5}
-            tag="h6"
-            altText={false}
-            color={ACCENT}
-            style={{
-              fontWeight: 700,
-              textAlign: "left",
-              textTransform: "uppercase",
-              margin: '2rem 0'
-            }}
-          >
-            TEST
-          </BodyText>
+          sizer={2}
+          tag="h6"
+          altText={true}
+          color={ACCENT}
+          style={{
+            fontWeight: 700,
+            textAlign: "left",
+            margin: "2rem 0"
+          }}
+        >
+          {project.headline}
+        </BodyText>
         <CategoryDivider />
-        <SupportBlock handleNavigation={() => this.props.history.push('/projects')} />
+        <BodyText
+          sizer={2}
+          tag="h6"
+          altText={false}
+          color={ACCENT}
+          style={{
+            fontWeight: "bolder",
+            textAlign: "left",
+            margin: "2rem 0"
+          }}
+        >
+          My Motivation?
+        </BodyText>
+        <BodyText
+          sizer={1.35}
+          tag="p"
+          altText={true}
+          color={ACCENT}
+          style={{
+            fontWeight: "bolder",
+            textAlign: "left",
+            margin: "2rem 0"
+          }}
+        >
+          {project.info}
+        </BodyText>
+        <div
+          css={`
+            margin-bottom: 2rem;
+            display: flex;
+            @media (max-width: 767px) {
+              flex-direction: column;
+              justify-content: flex-start;
+            }
+          `}
+        >
+          {project.demo ? (
+            <AnchorButton url={project.demo} text="Demo" minWidth="60" maxWidth="75" />
+          ) : null}
+          <AnchorButton url={project.repo} marginLeft={project.demo ? true : false} text="Source Code" minWidth="120" maxWidth="135"/>
+        </div>
+        <CategoryDivider />
+        <BodyText
+          sizer={2}
+          tag="h6"
+          altText={false}
+          color={ACCENT}
+          style={{
+            fontWeight: "bolder",
+            textAlign: "left",
+            margin: "2rem 0"
+          }}
+        >
+          How to Use?
+        </BodyText>
+        <BodyText
+          sizer={1.35}
+          tag="p"
+          altText={true}
+          color={ACCENT}
+          style={{
+            fontWeight: "bolder",
+            textAlign: "left",
+            margin: "2rem 0"
+          }}
+        >
+          {project.use}
+        </BodyText>
+        <CategoryDivider />
+        <SupportBlock
+          handleNavigation={() => this.props.history.push("/projects")}
+        />
         <SummaryCard />
-        <Footer activeItem={'Projects'} handleNavigation={() => {}} />
+        <Footer activeItem={"Projects"} handleNavigation={() => {}} />
       </div>
     );
   }
