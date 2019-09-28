44d8eb81902c5fb8b5637423366168688597b432
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-27 22:31:25 -0400

    removed unused

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Gitlab from '../assets/images/gitlab.png';
-import Forest from '../assets/images/forest.jpg';
 
-const Hero = ({ }) => {
+const Hero = () => {
     return (
-        <img css={`justify-self: center; max-height: 580px; width: 80%; max-width: 400px; min-height: 350px; min-width: 250px;`} src={Gitlab} alt="Profile Picture" />
+        <img css={`justify-self: center; max-height: 580px; width: 80%; max-width: 400px; min-height: 350px; min-width: 250px;`} src={Gitlab} alt="Profile" />
     )
 }
 
