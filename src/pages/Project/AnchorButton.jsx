cb1bb1041595fee5cfe8c10b9160e5d558bdcc93
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-27 22:31:59 -0400

    fix unused

---
+++ src/pages/Project/AnchorButton.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ACCENT, PRIMARY } from "../../theme";
+import BodyText from "../../components/BodyText";
+
+const AnchorButton = ({ url, text, minWidth, maxWidth, marginLeft }) => {
+    console.log(url)
+  return (
+    <a
+      href={url}
+      target="_target"
+      css={`
+        background: ${ACCENT};
+        text-decoration: none;
+        padding: 0 0.75rem;
+        min-width: ${minWidth}px;
+        max-width: ${maxWidth}px;
+        height: 45px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        fill: white;
+        color: white;
+        transition: all 200ms ease-in-out;
+        cursor: pointer;
+        margin-left: ${marginLeft ? "2rem" : null};
+        &:hover {
+          background: white;
+          fill: ${PRIMARY};
+          color: ${PRIMARY};
+        }
+        @media (max-width: 767px) {
+          height: 30px;
+          max-width: ${minWidth}px;
+          margin-left: 0;
+          margin-top: 1rem;
+        }
+      `}
+    >
+      <BodyText sizer={1.25} altText={true}>
+        {text}
+      </BodyText>
+    </a>
+  );
+};
+
+export default AnchorButton;
