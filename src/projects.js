f3af9700b14d0054fd712d55d857825447eda144
jon rinciari <rinciarijoc@gmail.com>
2019-09-28 19:19:05 -0400

    feat: Added other project information

--- src/projects.js
+++ src/projects.js
@@ -18,22 +18,30 @@ const MyProjects = {
     title: "React Native Swipeable Cards",
     subtext: "Component",
     logo: "",
-    headline: '',
-    info: ``,
-    use: '',
-    demo: '',
-    repo: ''
+    headline: 'Tinder style cards for react native. Allows for multi-directional panning so you can capture any movement needed!',
+    info: `
+      This was used for a company I worked for called Homereport. This idea of the application was a tinder style application
+      for homebuyers. There were not many swipeable card libraries that had smooth animations, customizable, and speedy.
+      It was the bread and butter of the application so by adapting depricated libraries and fixing bugs I was able to
+      publish a version that fit our needs.
+    `,
+    use: 'Install it using your favorite package manager, and add it to your application! You can begin swiping right away!',
+    demo: null,
+    repo: 'https://github.com/jonathanRinciari/React-Native-Swipeable-Cards'
   },
   "angular-grid-directive": {
     url: "angular-grid-directive",
     title: "Angular Grid Directive",
     subtext: "Angular Utility",
     logo: "",
-    headline: '',
-    info: ``,
-    use: '',
-    demo: '',
-    repo: ''
+    headline: 'Simple, Reactive, and Easy to use grid layout',
+    info: `I wanted to create a 12 column grid-layout directive for use in personal projects and professional use.
+    12 columns can be used to describe most layouts, and adding it as a directive lets you to easily build without
+    having to worry or keep track of classes. It is reactive so it will work for mobile or web!
+    `,
+    use: 'Inject the directive in your App module, and use it in your application how you see fit!',
+    demo: null,
+    repo: 'https://github.com/jonathanRinciari/angular-grid-layout'
   }
 };
 
