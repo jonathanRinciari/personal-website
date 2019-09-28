4039caa1887ffac7679462dd98455364f3ca2b2a
jon rinciari <jrinciari@jons-MacBook-Pro.local>
2019-09-27 22:31:21 -0400

    updated model

--- src/projects.js
+++ src/projects.js
@@ -3,19 +3,37 @@ const MyProjects = [
     url: "yelp-api",
     title: "YelpApi",
     subtext: "Nativescript Plugin",
-    logo: ""
+    logo: null,
+    headline: 'A nativescript plugin wrapper of the YelpFushion API. Use within your nativescript application to gain the power of Yelp.',
+    info: `I was building a nativescript application, and I wanted to use the yelp api in my app to find businesses given a users geolocation.
+    Unfortunately, even thought nativescript has a robust library of plugins the YelpApi was missing. I taught myself how to create plugins to
+    be injected into a nativescript application!
+    `,
+    use: `Simply inject into your application, and register using an ApiKey obtained from yelp.`,
+    demo: null,
+    repo: 'https://www.github.com/jonathanRinciari/nativescript-plugin-yelpapi'
   },
   {
     url: "react-native-swipeable-cards",
     title: "React Native Swipeable Cards",
     subtext: "Component",
-    logo: ""
+    logo: "",
+    headline: '',
+    info: ``,
+    use: '',
+    demo: '',
+    repo: ''
   },
   {
     url: "angular-grid-directive",
     title: "Angular Grid Directive",
     subtext: "Angular Utility",
-    logo: ""
+    logo: "",
+    headline: '',
+    info: ``,
+    use: '',
+    demo: '',
+    repo: ''
   }
 ];
 
