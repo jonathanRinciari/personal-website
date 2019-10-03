12016bd628547139d3322c3bdf4fbd3277966e2f
jon rinciari <rinciarijoc@gmail.com>
2019-10-02 21:10:38 -0400

    added blog post and asset

---
+++ src/pages/Writing/posts/surgical-consultant-to-full-stack-developer/meta.js
@@ -0,0 +1,16 @@
+export default {
+  path: "/surgical-consultant-to-developer",
+  cover: "rocky.jpeg",
+  date: "2018-03-28",
+  title: "Surgical Consultant To Full Stack Developer",
+  published: true,
+  tags: [
+    "developer",
+    "surgical consultant",
+    "full stack developer",
+    "freeCodeCamp",
+    "web developer",
+    "javascript",
+    "self-taught developer"
+  ]
+};
